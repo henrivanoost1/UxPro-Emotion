c300122c2c55989d1656d3682d058bf06d18a984
Henri Van Oost <henri.van.oost@student.howest.be>
2021-06-09 10:14:46 +0200

    setup main.py

--- main.py
+++ main.py
@@ -14,5 +14,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+def gen(camera):
+    pass
+
+
+    
+
+@app.route('/video_feed_test')
+def video_feed_test():
+    pass
+    
+
+    
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
