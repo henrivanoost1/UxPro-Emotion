557dea62bb6488eacd8eddc901c369f799c86273
Henri Van Oost <henri.van.oost@student.howest.be>
2021-06-09 10:09:44 +0200

    basic flask setup

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ import time
 import subprocess
 import cv2
 import os
+
+start_time=""
+end_time=""
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
