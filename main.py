446039e80d08175aefa941ac1a47472c7d18c2d7
Thibault D'Haese <thibault.dhaese@student.howest.be>
2021-06-15 23:58:33 +0200

    Delete some stuff

--- main.py
+++ main.py
@@ -243,16 +243,7 @@ def videosave():
         with open(os.path.join(path_data,"data"+str(count_json)+".json"),"w") as file:
             json.dump(list_moments,file,indent=4)
         
-        folder = 'last_data'
-        for filename in os.listdir(folder):
-            file_path = os.path.join(folder, filename)
-            try:
-                if os.path.isfile(file_path) or os.path.islink(file_path):
-                    os.unlink(file_path)
-                elif os.path.isdir(file_path):
-                    shutil.rmtree(file_path)
-            except Exception as e:
-                print('Failed to delete %s. Reason: %s' % (file_path, e))
+        
 
         # with open(os.path.join(path_data,"data"+str(count_json)+".json"),"w") as file:
         #     # file_data=json.load(file)
